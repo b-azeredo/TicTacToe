9453739e6ef4ca7f6929ee5d6827c1838bb17e10
Bernardo Azeredo <92824216+bernard0o@users.noreply.github.com>
2023-01-29 15:04:22 +0000

    .

--- script.js
+++ script.js
@@ -1,115 +1,57 @@
-let contador = 0;
 function add(a){
-    contador += 1;
-    if (contador % 2 == 0){
-        document.getElementById(a).textContent = "O";
-    }
-    else{
-        document.getElementById(a).textContent = "X";
+    if (quadrado[a].textContent == ""){
+        contador += 1;
+        if (contador % 2 == 0){
+            quadrado[a].textContent = "O";
+        }
+        else{
+            quadrado[a].textContent = "X";
+        }
     }
 }
-
-let q1 = document.getElementById("1");
-let q2 = document.getElementById("2");
-let q3 = document.getElementById("3");
-let q4 = document.getElementById("4");
-let q5 = document.getElementById("5");
-let q6 = document.getElementById("6");
-let q7 = document.getElementById("7");
-let q8 = document.getElementById("8");
-let q9 = document.getElementById("9");
 
 function reiniciar(){
     document.getElementById("winMessage").textContent = "Tic Tac Toe";
     contador = 0;
     document.getElementById("stop").style.display = "none";
-    q1.textContent = "";
-    q2.textContent = "";   
-    q3.textContent = "";   
-    q4.textContent = "";   
-    q5.textContent = "";   
-    q6.textContent = "";   
-    q7.textContent = "";   
-    q8.textContent = "";   
-    q9.textContent = "";   
+    for (let i = 0; i < 9; i++){
+        quadrado[i].textContent = "";
+    } 
 }
-
-q1.addEventListener("click", function(){add("1")});
-q2.addEventListener("click", function(){add("2")});
-q3.addEventListener("click", function(){add("3")});
-q4.addEventListener("click", function(){add("4")});
-q5.addEventListener("click", function(){add("5")});
-q6.addEventListener("click", function(){add("6")});
-q7.addEventListener("click", function(){add("7")});
-q8.addEventListener("click", function(){add("8")});
-q9.addEventListener("click", function(){add("9")});
 
 function winner(b){
-    document.getElementById("winMessage").textContent = `${b} ganhou`;
-    stop = document.getElementById("stop").style;
-    stop.display = "block";
+    document.getElementById("winMessage").textContent = `O jogador '${b}' ganhou`;
+    document.getElementById("stop").style.display = "block";
 }
 
+let contador = 0;
+let quadrado = document.getElementsByClassName("quadrado");
 let area = document.getElementById("areaJogo");
+for (let i2 = 0; i2<9; i2++){
+    quadrado[i2].addEventListener("click", function(){add(i2)})
+}
+
 area.addEventListener("click", function(){
     // Horizontal X
-    if (q1.textContent == "X" && q2.textContent == "X" && q3.textContent == "X"){
-        winner("X");
-    }
-    else if (q4.textContent == "X" && q5.textContent == "X" && q6.textContent == "X"){
-        winner("X");
-    }
-    else if (q7.textContent == "X" && q8.textContent == "X" && q9.textContent == "X"){
-        winner("X");
-    }
-
+    if (quadrado[0].textContent == "X" && quadrado[1].textContent == "X" && quadrado[2].textContent == "X"){winner("X");}
+    else if (quadrado[3].textContent == "X" && quadrado[4].textContent == "X" && quadrado[5].textContent == "X"){winner("X");}
+    else if (quadrado[6].textContent == "X" && quadrado[7].textContent == "X" && quadrado[8].textContent == "X"){winner("X");}
     // Horizontal O
-    if (q1.textContent == "O" && q2.textContent == "O" && q3.textContent == "O"){
-        winner("O");
-    }
-    else if (q4.textContent == "O" && q5.textContent == "O" && q6.textContent == "O"){
-        winner("O");
-    }
-    else if (q7.textContent == "O" && q8.textContent == "O" && q9.textContent == "O"){
-        winner("O");
-    }
-
+    if (quadrado[0].textContent == "O" && quadrado[1].textContent == "O" && quadrado[2].textContent == "O"){winner("O");}
+    else if (quadrado[3].textContent == "O" && quadrado[4].textContent == "O" && quadrado[5].textContent == "O"){winner("O");}
+    else if (quadrado[6].textContent == "O" && quadrado[7].textContent == "O" && quadrado[8].textContent == "O"){winner("O");}
     // Vertical X
-    if (q1.textContent == "X" && q4.textContent == "X" && q7.textContent == "X"){
-        winner("X");
-    }
-    else if (q2.textContent == "X" && q5.textContent == "X" && q8.textContent == "X"){
-        winner("X");
-    }
-    else if (q3.textContent == "X" && q6.textContent == "X" && q9.textContent == "X"){
-        winner("X");
-    }
-
+    if (quadrado[0].textContent == "X" && quadrado[3].textContent == "X" && quadrado[6].textContent == "X"){winner("X");}
+    else if (quadrado[1].textContent == "X" && quadrado[4].textContent == "X" && quadrado[7].textContent == "X"){winner("X");}
+    else if (quadrado[2].textContent == "X" && quadrado[5].textContent == "X" && quadrado[8].textContent == "X"){winner("X");}
     // Vertical O
-    if (q1.textContent == "O" && q4.textContent == "O" && q7.textContent == "O"){
-        winner("O");
-    }
-    else if (q2.textContent == "O" && q5.textContent == "O" && q8.textContent == "O"){
-        winner("O");
-    }
-    else if (q3.textContent == "O" && q6.textContent == "O" && q9.textContent == "O"){
-        winner("O");
-    }
-
+    if (quadrado[0].textContent == "O" && quadrado[3].textContent == "O" && quadrado[6].textContent == "O"){winner("O");}
+    else if (quadrado[1].textContent == "O" && quadrado[4].textContent == "O" && quadrado[7].textContent == "O"){winner("O");}
+    else if (quadrado[2].textContent == "O" && quadrado[5].textContent == "O" && quadrado[8].textContent == "O"){winner("O");}
     // Diagonal X
-    if (q1.textContent == "X" && q5.textContent == "X" && q9.textContent == "X"){
-        winner("X");
-    }
-    else if (q3.textContent == "X" && q5.textContent == "X" && q7.textContent == "X"){
-        winner("X");
-    }
+    if (quadrado[0].textContent == "X" && quadrado[4].textContent == "X" && quadrado[8].textContent == "X"){winner("X");}
+    else if (quadrado[2].textContent == "X" && quadrado[4].textContent == "X" && quadrado[6].textContent == "X"){winner("X");}
     // Diagonal O
-    if (q1.textContent == "O" && q5.textContent == "O" && q9.textContent == "O"){
-        winner("O");
-    }
-    else if (q3.textContent == "O" && q5.textContent == "O" && q7.textContent == "O"){
-        winner("O");
-    }
-
-
-})
+    if (quadrado[0].textContent == "O" && quadrado[4].textContent == "O" && quadrado[8].textContent == "O"){winner("O");}
+    else if (quadrado[2].textContent == "O" && quadrado[4].textContent == "O" && quadrado[6].textContent == "O"){winner("O");}
+});
